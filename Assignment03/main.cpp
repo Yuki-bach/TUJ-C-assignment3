9cdc0dd6d899fafa3b7479b9ba743fe4b93612ca
Yuki Ogawa <yuki20010221@icloud.com>
2021-06-04 15:13:43 +0900

    Initial Commit

---
+++ Assignment03/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment03
+//
+//  Created by Yuki Ogawa on 6/4/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
